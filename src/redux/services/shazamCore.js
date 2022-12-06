1eb68f7a2c7af3536285736411aa32ee1a325252
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2022-12-06 18:51:03 -0300

    SearchBar completed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,35 +1,59 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {
+    createApi,
+    fetchBaseQuery
+} from '@reduxjs/toolkit/query/react';
 
 
 export const shazamCoreApi = createApi({
-reducerPath: 'shazamCoreApi',
-baseQuery: fetchBaseQuery({
-    baseUrl:'https://shazam-core.p.rapidapi.com/v1',
-    prepareHeaders:(headers) => {
-        headers.set('X-RapidAPI-Key', 'cb9d57820amsh071d3fece3dbe5fp124bb9jsn20887519e01c');
-        return headers;
-    },
- }),
- endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () =>
-    '/charts/world'}),
-    getSongDetails: builder.query({query: ({songid}) =>
-    `/tracks/details?track_id=${songid}`}),
-    getSongRelated: builder.query({query: ({songid}) =>
-    `/tracks/related?track_id=${songid}`}),
-    getArtistDetails: builder.query({query: (artistId) =>
-    `/artists/details?artist_id=${artistId}`}),
-    getSongsByCountry: builder.query({query: (countryCode) =>
-        `/charts/country?country_code=${countryCode}`}),
-    getSongsByGenre: builder.query({query: (genre) =>
-            `/charts/genre-world?genre_code=${genre}`}),
- }),
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'cb9d57820amsh071d3fece3dbe5fp124bb9jsn20887519e01c');
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({
+            query: () =>
+                '/charts/world'
+        }),
+        getSongDetails: builder.query({
+            query: ({
+                    songid
+                }) =>
+                `/tracks/details?track_id=${songid}`
+        }),
+        getSongRelated: builder.query({
+            query: ({
+                    songid
+                }) =>
+                `/tracks/related?track_id=${songid}`
+        }),
+        getArtistDetails: builder.query({
+            query: (artistId) =>
+                `/artists/details?artist_id=${artistId}`
+        }),
+        getSongsByCountry: builder.query({
+            query: (countryCode) =>
+                `/charts/country?country_code=${countryCode}`
+        }),
+        getSongsByGenre: builder.query({
+            query: (genre) =>
+                `/charts/genre-world?genre_code=${genre}`
+        }),
+        getSongsBySearch: builder.query({
+            query: (searchTerm) =>
+                `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`
+        }),
+    }),
 });
-export const{
+export const {
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
     useGetSongsByCountryQuery,
     useGetSongsByGenreQuery,
+    useGetSongsBySearchQuery,
 } = shazamCoreApi;
