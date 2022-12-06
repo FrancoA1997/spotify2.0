36c73e49060f408e277f1912137f23f652768b02
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2022-12-06 19:19:51 -0300

    general customization

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,12 +12,12 @@ const NavLinks = ({handleClick}) => (
       key={item.name}
       to={item.to}
       className="
-      flex flex-row justify-start items-center
-       my-8 text-sm font-medium
+      flex flex-row justify-start pl-1 items-center
+       my-8 text-xl font-medium
       text-gray-400 hover:text-cyan-400"
       onClick={() => handleClick && handleClick()}
       >
-        <item.icon className="w-6 h-6 mr-2"/>
+        <item.icon className="w-7 h-7 mr-6"/>
         {item.name}
       </NavLink>
     ))}
@@ -28,7 +28,7 @@ const Sidebar = () => {
  return(
   <>
   <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-    <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+    <img src={logo} alt="logo" className="w-full h-40 object-contain" />
     <NavLinks/>
   </div>
   <div className="absolute md:hidden block top-6 right-3">
